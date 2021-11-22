5a20e28455b93f58322371a6550e25b98fd44762
Underscores <84211119+itsunderscores@users.noreply.github.com>
2021-11-22 00:52:36 -0600

    Update liker.php

--- liker.php
+++ liker.php
@@ -1,8 +1,10 @@
 <?php
+
 ob_start();
 error_reporting(0);
 set_time_limit(1200000);
 ini_set('max_execution_time', 1200000);
+include 'extra.php';
 
 /*
 Developed by Underscores
@@ -13,131 +15,32 @@ Syntax: php liker.php photography 10
 Go to Instagram, sign into your account, and copy your request headers and paste them into file "headers.txt"
 */
 
-function fix_headers() {
-	//Fix Headers
-	$rows = file("headers.txt");    
-	$blacklist = "gzip|content-length";
-	foreach($rows as $key => $row) {
-		if(preg_match("/($blacklist)/", $row)) {
-			unset($rows[$key]);
-		}
-	}
+clearStdin(); //Shitty way to clearn form on Windows lol
 
-	file_put_contents("headers.txt", implode("\n", $rows));
-	$file = file_get_contents('headers.txt');
-	$file = str_replace("\n", "", $file);
-	file_put_contents('headers.txt', $file);
-}
-fix_headers();
-
-function grab_feed($tag) {
-	
-	$headers1 = array("");
-	$headers = file_get_contents('headers.txt');
-	$headers1 = preg_split("/\r\n|\n|\r/", $headers);
-	//var_dump($headers1);
-	
-	$ch = curl_init();
-	curl_setopt($ch, CURLOPT_URL, 'https://www.instagram.com/explore/tags/'.$tag.'/?__a=1');
-	curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 10);
-	curl_setopt($ch, CURLOPT_TIMEOUT, 10);
-	curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
-	curl_setopt($ch, CURLOPT_HTTPHEADER, $headers1);
-	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-	$rt = curl_exec($ch);
-	$info = curl_getinfo($ch);
-	curl_close ($ch);
-	return $rt;
-}
-
-function grab_new_ids($tag, $max_id, $page) {
-	
-	$headers1 = array("");
-	$headers = file_get_contents('headers.txt');
-	$headers1 = preg_split("/\r\n|\n|\r/", $headers);
-	//var_dump($headers1);
-	
-	$ch = curl_init();
-	curl_setopt($ch, CURLOPT_URL, 'https://i.instagram.com/api/v1/tags/'.$tag.'/sections/');
-	curl_setopt($ch, CURLOPT_POST, 1);
-	curl_setopt($ch, CURLOPT_POSTFIELDS, 'include_persistent=0&max_id='.$max_id.'&page='.$page.'&surface=grid&tab=recent');
-	curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 10);
-	curl_setopt($ch, CURLOPT_TIMEOUT, 10);
-	curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
-	curl_setopt($ch, CURLOPT_HTTPHEADER, $headers1);
-	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-	$rt = curl_exec($ch);
-	$info = curl_getinfo($ch);
-	curl_close ($ch);
-	return $rt;
-}
-
-function like($id) {
-	
-	$headers1 = array("");
-	$headers = file_get_contents('headers.txt');
-	$headers1 = preg_split("/\r\n|\n|\r/", $headers);
-	//var_dump($headers1);
-	
-	$ch = curl_init();
-	curl_setopt($ch, CURLOPT_URL, 'https://www.instagram.com/web/likes/'.$id.'/like/');
-	curl_setopt($ch, CURLOPT_POST, 1);
-	curl_setopt($ch, CURLOPT_POSTFIELDS, '');
-	curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 10);
-	curl_setopt($ch, CURLOPT_TIMEOUT, 10);
-	curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
-	curl_setopt($ch, CURLOPT_HTTPHEADER, $headers1);
-	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-	$rt = curl_exec($ch);
-	$info = curl_getinfo($ch);
-	curl_close ($ch);
-	return $rt;
-}
-
-$green = "\e[92m";
-$white = "\e[97m";
-$red = "\e[95m";
-$yellow = "\e[93m";
-$blue = "\e[34m";
-
-function clearStdin()
-{
-    for ($i = 0; $i < 50; $i++) { echo "\r\n"; }
-}
-clearStdin();
-
-$tag = $_GET['tag']; //Tag
+//User Preferences
+$tag = $_GET['tag'];
 if($tag == null) {
 	$tag = $argv[1];
 	if($tag == null) {
 		echo $red."[*] Enter a tag!".$white; die();
 	}
 }
-
 $user_delay = $argv[2]; //User inputs this amount if they want to delay
-if($user_delay == null) { $user_delay = rand(10,18); $user_delay1 = "Randomized"; } else { $user_$delay1 = $user_delay; }
+if($user_delay == null) { $user_delay = rand(10,18); $user_delay1 = "Randomized"; } else { $user_delay1 = $user_delay; }
 
 
-echo $red."-----------------------------".$yellow."
-Instagram Liker v1
-Developed by Underscores
-Syntax: [TAG] [DELAY PER LIKE]
-".$red."-----------------------------".$yellow."
-Tag Entered: $tag
-Delay (Seconds): $user_delay1
-".$red."-----------------------------\n\n".$white;
-
-
+echo $message;
 $rt = grab_feed($tag); //Fetch Data
 $data = json_decode($rt, true); //Decode JSON
 $next_max = $data['data']['recent']['next_max_id']; //Next Max ID to scrape more images
 $array = array(""); //Setting blank array
 
-$_GET['top'] = "true";
-$_GET['recent'] = "true";
 
+
+
+/* Grab Top Posts (Should be 3 columns, 3 rows) */
+$_GET['top'] = "true"; $_GET['recent'] = "true";
 if($_GET['top'] == "true") {
-	//Grab Top Posts (Should be 3 columns, 3 rows)
 	echo $yellow."-----------------------------\n[+] Grabbing Top Posts\n-----------------------------\n".$white;
 	$events = $data['data']['top']['sections'];
 	foreach($events as $key=>$value)
@@ -156,15 +59,18 @@ if($_GET['top'] == "true") {
 		$pk_3 = $value['layout_content']['medias'][2]['media']['pk'];
 		$username_3 = $value['layout_content']['medias'][2]['media']['user']['username'];
 		array_push($array, $pk_3);
-		
-		//echo $green."$pk_1 ($username_1) - $pk_2 ($username_2) - $pk_3 ($username_3)".$white;
-		//ob_flush(); flush();
 	}
 	echo $green . "[-] Picture Queue: ". count($array) ."\n". $white;
 }
 
+
+
+
+
+
+
+/* Grab Recent Posts (Should be 10 columns, 3 rows) */
 if($_GET['recent'] == "true") {
-	//Grab Recent Posts (Should be 10 columns, 3 rows)
 	echo $yellow . "\n-----------------------------\n[-] Grabbing Recent Posts\n-----------------------------\n".$white;
 	$events = $data['data']['recent']['sections'];
 	foreach($events as $key=>$value)
@@ -184,14 +90,19 @@ if($_GET['recent'] == "true") {
 		$pk_3 = $value['layout_content']['medias'][2]['media']['pk'];
 		$username_3 = $value['layout_content']['medias'][2]['media']['user']['username'];
 		array_push($array, $pk_3);
-		//echo $green."$pk_1 ($username_1) - $pk_2 ($username_2) - $pk_3 ($username_3)".$white;
-		//ob_flush(); flush();
 	}
 	echo $green . "[-] Picture Queue: ". count($array) ."\n". $white;
 	ob_flush(); flush();
 }
 
-//Grab next couple of pages as well
+
+
+
+
+
+
+
+/* Grab next couple of pages as well */
 echo $yellow."\n-----------------------------\n[-] Grabbing more IDs...\n-----------------------------\n".$white;
 ob_flush(); flush();
 $current_page = 1;
@@ -245,8 +156,12 @@ for ($x = 1; $x <= 1; $x++) {
 	ob_flush(); flush();
 }
 
-echo $red."\n-----------------------------\n[!] Starting Likes\n-----------------------------\n".$white;
 
+
+
+/* Start Liking Images */
+$total_liked = 0;
+echo $red."\n-----------------------------\n[!] Starting Likes\n-----------------------------\n".$white;
 $likes = 0;
 foreach($array as $item) {
 	if($item != null) {
@@ -256,11 +171,13 @@ foreach($array as $item) {
 		//Check status of liked image
 		$decode_like = json_decode($send_like, true);
 		if($decode_like['status'] == "ok") {
-			echo "[+] Successfully liked picture $picture_id [". $likes + 1 ."/".count($array)."]\n";
+			$total_liked++;
+			echo "[+] Successfully liked picture $picture_id [". $likes + 1 ."/".count($array)."] [$total_liked] \n";
 			$likes++;
 		} else {
-			echo "\nUnexpected response: ".$decode_like['status']." Sleeping for a few minutes...\n";
-			Sleep(200);
+			echo "\n[!] Unexpected response from Instagram: ".$decode_like['status']." Sleeping for 5 minutes...\n";
+			ob_flush(); flush();
+			Sleep(300);
 		}
 		
 		//Flush
@@ -279,7 +196,14 @@ foreach($array as $item) {
 	}
 }
 
-//Scrape More Images, run in loop from here on out...
+
+
+
+
+
+
+
+/* Scrape More Images, run in loop from here... */
 while (true) {
     
 echo $yellow."\n-----------------------------\n[!] Scraping More Images ($current_page)\n-----------------------------\n".$white;
@@ -352,11 +276,13 @@ echo $yellow."\n-----------------------------\n[!] Scraping More Images ($curren
             //Check status of liked image
             $decode_like = json_decode($send_like, true);
             if ($decode_like['status'] == "ok") {
-			echo "[+] Successfully liked picture $picture_id [". $likes + 1 ."/".count($array)."]\n";
+				$total_liked++;
+				echo "[+] Successfully liked picture $picture_id [". $likes + 1 ."/".count($array)."] [$total_liked] \n";
                 $likes++;
             } else {
-				echo "\nUnexpected response: ".$decode_like['status']." Sleeping for a few minutes...\n";
-				Sleep(200);
+				echo "\n[!] Unexpected response from Instagram: ".$decode_like['status']." Sleeping for 5 minutes...\n";
+				ob_flush(); flush();
+				Sleep(300);
             }
             
             //Flush
@@ -366,12 +292,17 @@ echo $yellow."\n-----------------------------\n[!] Scraping More Images ($curren
 			if($likes % 50 == 0) {
 				$delay = rand(30,60);
 				echo "[*] Waiting $delay seconds before continuing...\n"; ob_flush(); flush();
+				ob_flush(); flush();
 				sleep($delay); //Delay
 			} else {
 				sleep($user_delay); //Sleep for random seconds
 			}
-            ob_flush(); flush();
+            
+            //echo str_pad('', 4096) . "\n"; ob_flush(); flush();
         }
     }
+
+    //Sleep before getting new list.
+    sleep(rand(30,60));
 }
 ?>
